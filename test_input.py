9af0d9768b3fb5b05a85fc81a5c29336a90ccd2a
lkimura11 <lkimura@berkeley.edu>
2025-02-19 15:08:21 -0800

    Update test_input.py

--- test_input.py
+++ test_input.py
@@ -1,7 +1,15 @@
 import sys 
 import time
 import csv
-
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+import serial
+from adafruit_pm25.uart import PM25_UART
+import adafruit_bme680
+import board
 
 ## Argument
 if len(sys.argv) < 2:
@@ -11,91 +19,82 @@ if len(sys.argv) < 2:
 else:
   run_time = int(sys.argv[1])
 
+file = open('pm2.5_and_weather_data', 'w', newline = None)
+csvwriter = csv.writer(file, delimiter=',')
+
 count=0
 while count < run_time:
   count += 1
   print("Taking data entry:", count)
   time.sleep(1)
 
-file = open('pm2.5_and_weather_data', 'w', newline = None)
-csvwriter = csv.writer(file, delimiter=',')
-
-## Weather
-import adafruit_bme680
-import board
-
-# Create sensor object, communicating over the board's default I2C bus
-i2c = board.I2C()   # uses board.SCL and board.SDA
-bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
-
-# change this to match the location's pressure (hPa) at sea level
-bme680.sea_level_pressure = 1013.25
-
-while True:
-  print("\nTemperature: %0.1f C" % bme680.temperature)
-  print("Gas: %d ohm" % bme680.gas)
-  print("Humidity: %0.1f %%" % bme680.relative_humidity)
-  print("Pressure: %0.3f hPa" % bme680.pressure)
-  print("Altitude = %0.2f meters" % bme680.altitude)
-  csvwriter.writerows([[bme680.temperature],[bme680.gas],[bme680.relative_humidity],[bme680.pressure],[bme680.altitude]])
+  ## Weather
+ 
   
-  time.sleep(2)
+  # Create sensor object, communicating over the board's default I2C bus
+  i2c = board.I2C()   # uses board.SCL and board.SDA
+  bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+  
+  # change this to match the location's pressure (hPa) at sea level
+  bme680.sea_level_pressure = 1013.25
 
-
-## PM 2.5
-
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-"""
-Example sketch to connect to PM2.5 sensor with either I2C or UART.
-"""
-
-# pylint: disable=unused-import
-import time
-import board
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-from adafruit_pm25.i2c import PM25_I2C
-
-
-reset_pin = None
-# If you have a GPIO, its not a bad idea to connect it to the RESET pin
-# reset_pin = DigitalInOut(board.G0)
-# reset_pin.direction = Direction.OUTPUT
-# reset_pin.value = False
-
-
-# For use with a computer running Windows:
-# import serial
-# uart = serial.Serial("COM30", baudrate=9600, timeout=1)
-
-# For use with microcontroller board:
-# (Connect the sensor TX pin to the board/computer RX pin)
-# uart = busio.UART(board.TX, board.RX, baudrate=9600)
-
-# For use with Raspberry Pi/Linux:
-import serial
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
-
-# For use with USB-to-serial cable:
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
-
-# Connect to a PM2.5 sensor over UART
-from adafruit_pm25.uart import PM25_UART
-pm25 = PM25_UART(uart, reset_pin)
-
-# Create library object, use 'slow' 100KHz frequency!
-# i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-# pm25 = PM25_I2C(i2c, reset_pin)
-
-print("Found PM2.5 sensor, reading data...")
-
-while True:
+  ## PM 2.5
+  
+  # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+  # SPDX-License-Identifier: MIT
+  
+  """
+  Example sketch to connect to PM2.5 sensor with either I2C or UART.
+  """
+  
+  # pylint: disable=unused-import
+ 
+  
+  
+  reset_pin = None
+  # If you have a GPIO, its not a bad idea to connect it to the RESET pin
+  # reset_pin = DigitalInOut(board.G0)
+  # reset_pin.direction = Direction.OUTPUT
+  # reset_pin.value = False
+  
+  
+  # For use with a computer running Windows:
+  # import serial
+  # uart = serial.Serial("COM30", baudrate=9600, timeout=1)
+  
+  # For use with microcontroller board:
+  # (Connect the sensor TX pin to the board/computer RX pin)
+  # uart = busio.UART(board.TX, board.RX, baudrate=9600)
+  
+  # For use with Raspberry Pi/Linux:
+  
+  uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+  
+  # For use with USB-to-serial cable:
+  # import serial
+  # uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
+  
+  # Connect to a PM2.5 sensor over UART
+  
+  pm25 = PM25_UART(uart, reset_pin)
+  
+  # Create library object, use 'slow' 100KHz frequency!
+  # i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+  # Connect to a PM2.5 sensor over I2C
+  # pm25 = PM25_I2C(i2c, reset_pin)
+  
+  print("Found PM2.5 sensor, reading data...")
+  
+  while True:
+    print("\nTemperature: %0.1f C" % bme680.temperature)
+    print("Gas: %d ohm" % bme680.gas)
+    print("Humidity: %0.1f %%" % bme680.relative_humidity)
+    print("Pressure: %0.3f hPa" % bme680.pressure)
+    print("Altitude = %0.2f meters" % bme680.altitude)
+    csvwriter.writerows([[bme680.temperature],[bme680.gas],[bme680.relative_humidity],[bme680.pressure],[bme680.altitude]])
+    
     time.sleep(1)
-
+  
     try:
         aqdata = pm25.read()
         # print(aqdata)
@@ -127,5 +126,5 @@ while True:
   
     csvwriter.writerow([aqdata["pm25 standard"]])
     
-file.close()
-
+  file.close()
+  
