6ebea43c3ea358cbe3343a51829db737910966d9
lkimura11 <lkimura@berkeley.edu>
2025-02-19 14:38:03 -0800

    Create test_input.py

---
+++ test_input.py
@@ -0,0 +1,17 @@
+import sys 
+import time
+
+
+
+if len(sys.argv) < 2:
+  print("This script requires an input argument specifying the run time in seconds")
+  exit()
+  #run_time = 10
+else:
+  run_time = int(sys.argv[1])
+
+count=0
+while count < run_time:
+  count += 1
+  print("Taking data entry:", count)
+  time.sleep(1)
