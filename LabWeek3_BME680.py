71d700b6d2fa0a688867a305b5d012a91babb955
lkimura11 <lkimura@berkeley.edu>
2025-03-04 10:07:21 -0800

    Update and rename Weather.py to LabWeek3_BME680.py

--- LabWeek3_BME680.py
+++ LabWeek3_BME680.py
@@ -7,13 +7,23 @@ i2c = board.I2C()   # uses board.SCL and board.SDA
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
-bme680.sea_level_pressure = 1013.25
+bme680.sea_level_pressure = 1022
 
-while True:
+start_time = time.time()
+
+count = 0
+while count < 20:
+
+    count+=1
+
+    elapsed_time = time.time() - start_time
+
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    
     print("\nTemperature: %0.1f C" % bme680.temperature)
     print("Gas: %d ohm" % bme680.gas)
     print("Humidity: %0.1f %%" % bme680.relative_humidity)
     print("Pressure: %0.3f hPa" % bme680.pressure)
     print("Altitude = %0.2f meters" % bme680.altitude)
 
-    time.sleep(2)
+    time.sleep(1)
