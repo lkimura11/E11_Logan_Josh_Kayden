ebb08c5978ef383e4385903870c5a0b69e937337
kaydenroloff <kayden@berkeley.edu>
2025-04-02 14:22:00 -0700

    Create Week11.py

---
+++ Week11.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime
+
+PIN_NUM = 17
+count = 0
+log_file = "radiation_count_in.txt"
+
+def log_event(event):
+    with open(log_file, "a") as f:
+        f.write(event + "\n")
+
+def count_num(channel):
+    global count
+    count += 1
+    print('\n ▲ at ' + str(datetime.now())) 
+    log_event(f"{datetime.now()} - Pulse detected")
+    
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_NUM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(PIN_NUM, GPIO.FALLING, callback=count_num)
+
+try: 
+     while True:
+        time.sleep(60) 
+        print(f"Counts in the last minute were: {count}")
+        log_event(f"{datetime.now()} - Counts in the last minute: {count}")
+        count = 0 #reset
+
+  
+except KeyboardInterrupt:
+    print("\nExiting... Cleaning up GPIO.") 
+    GPIO.cleanup()
+print("Done!")
